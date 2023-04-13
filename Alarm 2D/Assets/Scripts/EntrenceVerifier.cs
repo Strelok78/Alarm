7859fb558fb38c88b0bd25a4bdd699871ccf59a5
Shota <eg.shota@yandex.ru>
2023-04-13 06:23:39 +0300

    Trigger updated in EntrenceVerifier
    OnTrigger checks if collision has an Enemy script

--- Alarm 2D/Assets/Scripts/EntrenceVerifier.cs
+++ Alarm 2D/Assets/Scripts/EntrenceVerifier.cs
@@ -1,29 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.Events;
 
 
 public class EntrenceVerifier : MonoBehaviour
 {
-    private const string EnemyLayer = "Enemy";
-
-    [SerializeField] private LayerMask _layerMask;
     [SerializeField] private UnityEvent _entered;
     [SerializeField] private UnityEvent _left;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.TryGetComponent(Movement))
+        if (collision.TryGetComponent<Enemy>(out _))
             _entered.Invoke();
-
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.layer == LayerMask.NameToLayer(EnemyLayer))
-        {
-            _left.Invoke();
-        }
+        if (collision.TryGetComponent<Enemy>(out _))
+            _left.Invoke();        
     }
 }
